94dbfc5e8099f5fbbdac4006b78696b508549c54
Jalayana <millerja14@outlook.com>
2025-05-28 10:27:07 +0800

    Added footer with links to depository and hosting website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>My React Weather App</h1>
+      <footer>
+        This app was coded by Jalayana Miller. It is open-sourced on{" "}
+        <a
+          href="https://github.com/Jalayana/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://soft-concha-da7918.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
